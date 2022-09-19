3b1ffc9a73dce2453ff49621a45e94b6a2cc67be
ruihanWang <1522662630@163.com>
2022-09-19 22:49:41 +0800

    第四次提交 增加了去除文章标点符号的功能,可能对文章识别的准确率有帮助

--- main.py
+++ main.py
@@ -2,17 +2,16 @@
 import jieba
 from jieba import analyse
 from line_profiler_pycharm import profile
+import os
+import re
 from simhash import Simhash
+
+
+
+
 
 @profile
 def simhash_similarity(text1, text2):
-
-    #使用jieba进行关键词提取   不够准确
-    '''word1 = analyse.extract_tags(text1,topK=800)
-    word2 = analyse.extract_tags(text2,topK=800)
-    print(word1)
-    print(word2)'''
-
     word1 = jieba.lcut(text1, cut_all=False, HMM=False)
     word2 = jieba.lcut(text2, cut_all=False, HMM=False)
     print(word1)
@@ -27,28 +26,58 @@ def simhash_similarity(text1, text2):
     #取得两个hash值中长度较大的作为参考长度
     hashbit = max(len(bin(a_simhash.value)),len(bin(b_simhash.value)))
 
-    #计算两个hash值之间的海明距
+    #计算两个hash值之间的汉明距
     distince = a_simhash.distance(b_simhash)
     print("两文章Simhash处理后的汉明距离为:", distince)
     similar = (1-(distince / hashbit))*100
+
+    #如果两篇文章相似度不高,使用分词会导致相似度虚高,所以使用关键词提取来判断文本相似度
+    if(similar<=80):
+        word1 = analyse.extract_tags(text1, topK=800)
+        word2 = analyse.extract_tags(text2, topK=800)
+        a_simhash = Simhash(word1)
+        b_simhash = Simhash(word2)
+        distince = a_simhash.distance(b_simhash)
+        similar = (1-(distince / hashbit)) * 100
+
     return similar
 
+#去除标点符号
+def remove_punctuation(data):
+    data1 = []
+    remove_chars = '[·’!"\#$%&\'()＃！（）*+,-./:;<=>?\@，：?￥★、…．＞【】［］《》？“”‘’\[\\]^_`{|}~]+'
+    string = re.sub(remove_chars, "", data)
+    data1.append(string)
+    return data1
 @profile
 def main():
+
+
+    file1 = '测试文本/orig.txt'
+    f1 = open(file1, encoding='utf-8')
+
+    file2 = '测试文本/three bodies.txt'
+    f2 = open(file2, encoding='utf-8')
+
     # 将原文文本导入到程序中去
-    f1 = open('测试文本/orig.txt', encoding='utf-8')
-    data1 = f1.read()
+    data1_notremove = f1.read()
+    data1_ = remove_punctuation(data1_notremove)
+    data1 = str(data1_)
+    f1.close()
+
 
     # 将鉴定抄袭文本导入到程序中去
-    f2 = open('测试文本/orig_0.8_dis_10.txt', encoding='utf-8')
-    data2 = f2.read()
-
+    data2_notremove =f2.read()
+    data2_ = remove_punctuation(data2_notremove)
+    data2 = str(data2_)
+    f2.close()
 
     similary = simhash_similarity(data1, data2);
-    print("文本相似度:", similary)
+    print("论文原文的绝对路径:", os.path.abspath(file1))
+    print("抄袭论文的绝对路径:", os.path.abspath(file2))
+    print("文本相似度:", similary,"%")
 
-    f1.close()
-    f2.close()
+
 
 #运行程序
 main()
